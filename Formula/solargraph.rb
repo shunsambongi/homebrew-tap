64a09531465975858428f8ceb8766d184c52684b
shunsambongi <15838820+shunsambongi@users.noreply.github.com>
2021-12-02 05:09:26 +0000

    solargraph: add 0.44.2 bottle.

--- Formula/solargraph.rb
+++ Formula/solargraph.rb
@@ -7,6 +7,12 @@ class Solargraph < Formula
       revision: "e50a6dc4b43e2183e245aded8d23f0003d8c6bf7"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/shunsambongi/homebrew-tap/releases/download/solargraph-0.44.2"
+    sha256                               big_sur:      "e9b79c00231fc7a9200a722c2764f5268fcf2d33251e9d667baf43c4e5afa773"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "137e5f776203afe9c4b6381d1ec721b9a23af159a1c5306ece172c54553456d0"
+  end
+
   uses_from_macos "ruby"
 
   def install
