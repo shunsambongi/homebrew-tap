8db945321a7e45abaa7ce8ebe120edef04f5ec24
Shun Sambongi <shunsambongi@gmail.com>
2023-01-12 00:10:07 -0700

    dockerfile-language-server-nodejs 0.9.0
    Closes #24.
    
    Signed-off-by: shunsambongi <15838820+shunsambongi@users.noreply.github.com>

--- Formula/dockerfile-language-server-nodejs.rb
+++ Formula/dockerfile-language-server-nodejs.rb
@@ -3,8 +3,8 @@ require "language/node"
 class DockerfileLanguageServerNodejs < Formula
   desc "Language server for Dockerfiles"
   homepage "https://github.com/rcjsuen/dockerfile-language-server-nodejs"
-  url "https://registry.npmjs.org/dockerfile-language-server-nodejs/-/dockerfile-language-server-nodejs-0.7.2.tgz"
-  sha256 "4318de559b9dedd40f1dc6e181f59bd10a3c4a37a84e0260f14fa340aa3b0952"
+  url "https://registry.npmjs.org/dockerfile-language-server-nodejs/-/dockerfile-language-server-nodejs-0.9.0.tgz"
+  sha256 "88a6085ca049ebdf383f3644ee699e982379e3767918bf14baedd0ef90c1e18f"
   license "MIT"
 
   bottle do
@@ -21,100 +21,24 @@ class DockerfileLanguageServerNodejs < Formula
   end
 
   test do
-    require "json"
     require "open3"
 
-    def send_request(stdin, json)
-      request = "Content-Length: #{json.size}\r\n\r\n#{json}"
-      puts request
-      stdin.write request
-    end
-
-    def send_notification(stdin, json)
-      notification = "Content-Length: #{json.size}\r\n\r\n#{json}"
-      puts notification
-      stdin.write notification
-    end
-
-    def handle_response(id, stdout)
-      header = nil
-      json = nil
-      response = nil
-      loop do
-        header = stdout.readline.chomp
-        pattern = /Content-Length: (?<length>\d+)/
-        assert_match pattern, header
-
-        parts = header.match pattern
-        content_length = Integer(parts["length"])
-
-        spacer = stdout.readline.chomp
-        assert_equal "", spacer
-
-        json = stdout.readpartial(content_length)
-
-        response = JSON.parse(json)
-
-        break if response["id"] == id
-      end
-      puts "#{header}\r\n\r\n#{json}"
-      response
-    end
-
-    exit_status = nil
-    Open3.popen3("#{bin}/docker-langserver", "--stdio") do |stdin, stdout, _, wait_thr|
-      ohai "Sending initialize request"
-      send_request stdin, <<~JSON
-        {
-          "jsonrpc": "2.0",
-          "id": 1,
-          "method": "initialize",
-          "params": {
-            "rootUri": null,
-            "capabilities": {}
-          }
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
         }
-      JSON
+      }
+    JSON
 
-      ohai "Receiving initialize response"
-      handle_response 1, stdout do |response|
-        assert !response.key?("error"), "Response should not have 'error' key"
-      end
-
-      ohai "Sending initialized notification"
-      send_notification stdin, <<~JSON
-        {
-          "jsonrpc": "2.0",
-          "method": "initialized"
-        }
-      JSON
-
-      ohai "Sending shutdown request"
-      send_request stdin, <<~JSON
-        {
-          "jsonrpc": "2.0",
-          "id": 2,
-          "method": "shutdown",
-          "params": null
-        }
-      JSON
-
-      ohai "Receiving shutdown response"
-      handle_response 2, stdout do |response|
-        assert !response.key?("error"), "Response should not have 'error' key"
-      end
-
-      ohai "Sending exit notification"
-      send_notification stdin, <<~JSON
-        {
-          "jsonrpc": "2.0",
-          "method": "exit"
-        }
-      JSON
-
-      ohai "Waiting for language server to exit"
-      exit_status = wait_thr.value
+    Open3.popen3("#{bin}/docker-langserver", "--stdio") do |stdin, stdout, _, _|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
     end
-    assert exit_status.success?
   end
 end
