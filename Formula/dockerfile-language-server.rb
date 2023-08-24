5b0698b95114643f9a1aa82f1dec2c7405982186
shunsambongi <15838820+shunsambongi@users.noreply.github.com>
2023-08-24 08:08:20 +0000

    dockerfile-language-server: update 0.9.0 bottle.

--- Formula/dockerfile-language-server.rb
+++ Formula/dockerfile-language-server.rb
@@ -8,9 +8,9 @@ class DockerfileLanguageServer < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/shunsambongi/homebrew-tap/releases/download/dockerfile-language-server-nodejs-0.9.0"
-    sha256 cellar: :any_skip_relocation, monterey:     "30263d48331a33fa8b3e4fa9def410946d0f36d21dadadbc22678324c81131c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3dba82ad0e5823839c5e2f5c8e32f8f06da67f2710c5cc0d937ebd54fd9e7713"
+    root_url "https://github.com/shunsambongi/homebrew-tap/releases/download/dockerfile-language-server-0.9.0"
+    sha256 cellar: :any_skip_relocation, monterey:     "66e0006b1d5a58ae5b1a6163be9d320366b9eb67b33d9902c868ddd37ee951d3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0cbf19af71c82815dca21168f329cca6ed9dad1f1266184716dcc57780508aae"
   end
 
   depends_on "node"
